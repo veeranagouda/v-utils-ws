5db38b46385db35cdeecbe98edcce1ef655bba41
Veeranagouda R M <veeranagouda@gmail.com>
2014-12-02 17:13:09 +0530

    Added date ws.

---
+++ src/main/java/com/veeru/util/ws/Utils.java
@@ -0,0 +1,44 @@
+package com.veeru.util.ws;
+
+import com.google.gson.Gson;
+import org.apache.log4j.Logger;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.TimeZone;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: veerana
+ * Date: 12/2/14
+ * Time: 5:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Path("platform/utils")
+public class Utils {
+    private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+
+    @GET
+    @Path("/date")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public Response sayDate(@DefaultValue("IST") @QueryParam("zone") String timeZone){
+
+        String[] timeZones={"IST","PDT","EDT","UTC","CST","EST","JTZ"};
+        HashMap<String,String> timeMap=new HashMap<>();
+        long currentTimeInMillis = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        for(String zone:timeZones){
+            TimeZone tZone = TimeZone.getTimeZone(zone);
+            sdf.setTimeZone(tZone);
+            Date resultDate =  new Date(currentTimeInMillis);
+            timeMap.put(zone,sdf.format(resultDate));
+        }
+        Gson gson=new Gson();
+        String message=gson.toJson(timeMap,HashMap.class);
+        return Response.ok(message, MediaType.APPLICATION_JSON).build();
+    }
+}
